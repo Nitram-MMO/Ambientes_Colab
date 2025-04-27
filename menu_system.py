1846ba42ab221e873323f7f1d309d0b4082c8398
Martin Mahecha <martinmahecha80@gmail.com>
2025-04-27 14:33:58 +0100

    Constats used for menu, dishes, days initialized and list filled with values.

--- menu_system.py
+++ menu_system.py
@@ -1,3 +1,17 @@
 import os
 import json
 import random
+
+
+#constants to be used 
+
+USER_FILE = "user.json"
+MENU_FILE = "menu.json"
+ORDERS_FILE = "orders.json"
+FIXED_PRICE = 2.80
+
+DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+meat_dishes = ["Steak", "Chicken Curry", "Pork Chops", "Beef Stew", "Grilled Lamb"]
+fish_dishes = ["Grilled Salmon", "Fish and Chips", "Tuna Salad", "Shrimp Pasta", "Baked Cod"]
+vegetarian_dishes = ["Veggie Burger", "Pasta Primavera", "Grilled Vegetables", "Tofu Stir-fry", "Vegetable Curry"]
